a565611d149519b257a51a55c042d22cdac53b8e
d9nich <dan.galaiko@gmail.com>
2020-06-23 16:34:23 +0300

    A Screen-Scrapping Program Using Beautiful Soup.

--- job_geter.py
+++ job_geter.py
@@ -1,39 +1,11 @@
 from urllib.request import urlopen
-from html.parser import HTMLParser
+from bs4 import BeautifulSoup
 
+text = urlopen('http://python.org/jobs').read()
+soup = BeautifulSoup(text, 'html.parser')
 
-def is_job(url):
-    try:
-        a, b, c, d = url.split('/')
-    except ValueError:
-        return False
-    return a == d == '' and b == 'jobs' and c.isdigit()
+jobs = set()
+for job in soup.body.section('h2'):
+    jobs.add('{} ({})'.format(job.a.string, job.a['href']))
 
-
-class Scraper(HTMLParser):
-
-    in_link = False
-
-    def handle_starttag(self, tag, attrs):
-        attrs = dict(attrs)
-        url = attrs.get('href', '')
-
-        if tag == 'a' and is_job(url):
-            self.url = url
-            self.in_link = True
-            self.chunks = []
-
-    def handle_data(self, data):
-        if self.in_link:
-            self.chunks.append(data)
-
-    def handle_endtag(self, tag):
-        if tag == 'a' and self.in_link:
-            print('{} ({})'.format(''.join(self.chunks), self.url))
-            self.in_link = False
-
-
-text = urlopen('http://python.org/jobs').read().decode()
-parser = Scraper()
-parser.feed(text)
-parser.close()
+print('\n'.join(sorted(jobs, key=str.lower)))
